e6eb3ff16c88b34a7ca2feea88ce9ac42247faf6
Blanc <Apurejing@163.com>
2022-01-05 23:23:47 +0800

    es6

--- src/jsday09.js
+++ src/jsday09.js
@@ -280,3 +280,58 @@ const sum1 = arr4.reduce(function(previousValue, item, index, arr) {
 }, 0)
 
 
+// 使用模板字符串的反引号的语法创建一个包含条目（li）字符串的数组。 每个条目应该是 result 对象 failure 属性的数组内的元素，并具有 class 属性，值为 text-warning。 makeList 函数应该返回列表项字符串的数组。
+
+// 使用遍历方法（可以是任意形式的循环）输出指定值（如下）。
+
+[
+  '<li class="text-warning">no-var</li>',
+  '<li class="text-warning">var-on-top</li>',
+  '<li class="text-warning">linebreak</li>'
+]
+
+// Solutions
+// Solution 1 (Click to Show/Hide)
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  "use strict";
+  // change code below this line
+  const failureItems = [];
+  for (let i = 0; i < arr.length; i++) {
+    failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+  }
+  // change code above this line
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
+
+
+// Solution 2 (Click to Show/Hide)
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  "use strict";
+  // change code below this line
+  const failureItems = arr.map(item => `<li class="text-warning">${item}</li>`);
+  // change code above this line
+  return failureItems;
+}
+const failuresList = makeList(result.failure);
+
+const profileUpdate = function(profileData) {
+    const { name, age } = profileData;
+}
+
+
+
+const profileData1 = function(  {name, age} ) {
+
+}
