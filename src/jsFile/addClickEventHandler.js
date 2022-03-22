82b5ba444efcb2f4518c23d102ea6f227c0ef8d9
Blanc <Apurejing@163.com>
2022-03-23 00:12:03 +0800

    fix: 文件变动

--- src/jsFile/addClickEventHandler.js
+++ src/jsFile/addClickEventHandler.js
@@ -3,10 +3,12 @@ const targetEle = document.getElementById('box');
 console.log('目标元素--', targetEle);
 //  监听目标元素的点击事件
 targetEle.addEventListener('click', function (event) {
-    console.log('点击事件发生了，浏览器给的事件对象--', event);
-    console.log('点击事件发生的元素--', event.target, event.target.classList);
-    console.log('监听器所在的元素--', event.currentTarget);
-    // 如果event.target的classList上，有greenInnerBox类名，那么移除greenInnerBox
-    //  如果没有，那么添加
-    event.target.classList.toggle('greenInnerBox');
-})
+  console.log('点击事件发生了，浏览器给的事件对象--', event);
+  console.log('点击事件发生的元素--', event.target, event.target.classList);
+  console.log('监听器所在的元素--', event.currentTarget);
+  // 如果event.target的classList上，有greenInnerBox类名，那么移除greenInnerBox
+  //  如果没有，那么添加
+  event.target.classList.toggle('greenInnerBox');
+});
+
+//  浏览器事件循环系统还是很不错的
